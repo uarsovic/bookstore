665a0a1382f8a3b166035dd65f442775738bdc63
uarsovic <uros.arsovic@adition.com>
2025-10-03 15:34:47 +0200

    Fix missing method security annotation

--- src/main/kotlin/rs/ua/bookstore/config/SecurityConfig.kt
+++ src/main/kotlin/rs/ua/bookstore/config/SecurityConfig.kt
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.core.convert.converter.Converter
 import org.springframework.security.authentication.AbstractAuthenticationToken
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
@@ -29,6 +30,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
 @Profile("default")
 class SecurityConfig {
 
